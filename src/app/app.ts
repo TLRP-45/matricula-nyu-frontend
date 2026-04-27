57225cafa3f0359645839c5c515dadfb2b945550
Antonella Butron <antonella.butron.fuentes@alumnos.uta.cl>
2026-04-26 21:05:47 -0400

    fix: ocultar el header en la vista de login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router'; 
+import { RouterOutlet, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private router: Router) {}
+
+  showHeader(): boolean {
+    return this.router.url !== '/login';
+  }
+}
